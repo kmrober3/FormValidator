4a1110adb4013cac0da557c5c74014764cce2eb5
kmrober3 <kmroberts@unc.edu>
2025-09-12 15:13:27 -0400

    Added working logic in JS file

--- formvalidation.js
+++ formvalidation.js
@@ -4,11 +4,55 @@ class FormValidation {
         let userInfo = new Map();
     } 
 
-    validateInfo() {
+    validateInfo(username, password) {    
+        if (username.length < 0 || password.length < 0) {
+            return false;
+        }  
 
+        if (username.length < 4 || password.length < 6) {
+            return false;
+        }   
+
+        if (username.length > 20) {
+            return false;
+        }
+
+        let usernameIsValid = this.checkUserName(username);
+        let passwordIsValid = this.checkPassword(password); 
+
+        return usernameIsValid && passwordIsValid;
+    }    
+
+    checkUserName(username) {
+        for (let i = 0; i < username.length; i++) {
+            if (!this.isLetterOrDigit(username[i])) {
+                return false;
+            }
+        } 
+        return true;
+    }  
+
+    checkPassword(password) {
+        let count = 0; 
+        for (let i = 0; i < password.length; i++) {
+            if (this.isDigit(password[i])) {
+                count++;
+            }
+        } 
+        return count > 0;
     }
+
+    isLetterOrDigit(char) {
+        return /^[a-zA-Z0-9]$/.test(char);
+    } 
+
+    isDigit(char) {
+        return /^[0-9]$/.test(char);
+    }
+
 } 
 
+let instance = new FormValidation();
 
 // Testing Code 
 let readline = require("readline");
@@ -18,6 +62,9 @@ let rl = readline.createInterface({
     output: process.stdout,
 }); 
 
-rl.question("Enter Username: ", (answer) => ({ 
-    // ToDo
-}))
+rl.question("Enter Username: ", (answer) => {  
+    rl.question("Enter Password: ", (p) => { 
+        console.log(instance.validateInfo(answer, p));  
+        rl.close();
+    })  
+});
